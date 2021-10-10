430339b33ca1fa7c92dd2d1126cef61fd8fa22a2
Shalllashaska <nickita.burov@gmail.com>
2021-10-10 23:20:17 +0300

    12

---
+++ www/js/scrollProjects.js
@@ -0,0 +1,49 @@
+var isScrolling = false;
+
+window.addEventListener("scroll", throttleScroll, false);
+
+function throttleScroll(e) {
+  if (isScrolling == false) {
+    window.requestAnimationFrame(function () {
+      scrolling(e);
+      isScrolling = false;
+    });
+  }
+  isScrolling = true;
+}
+
+document.addEventListener("DOMContentLoaded", scrolling, false);
+
+var listItems = document.querySelectorAll(".content");
+
+
+function scrolling(e) {
+  if (window.focus) {
+    for (var i = 0; i < listItems.length; i++) {
+      var listItem = listItems[i];
+
+      if (isPartiallyVisible(listItem)) {
+        listItem.classList.add("_anim");
+      }
+    }
+  }
+}
+
+function isPartiallyVisible(el) {
+  var elementBoundary = el.getBoundingClientRect();
+
+  var top = elementBoundary.top;
+  var bottom = elementBoundary.bottom;
+  var height = elementBoundary.height;
+
+  return (((top + height) / 2 >= 0) && ((height + window.innerHeight) / 2 >= bottom));
+}
+
+function isFullyVisible(el) {
+  var elementBoundary = el.getBoundingClientRect();
+
+  var top = elementBoundary.top;
+  var bottom = elementBoundary.bottom;
+
+  return ((top >= 0) && (bottom <= window.innerHeight));
+}
